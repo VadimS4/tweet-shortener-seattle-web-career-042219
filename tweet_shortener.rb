8a3cd6b230dc7174221a061c8987cef7a3293c3f
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 23:01:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,6 +41,8 @@ def shortened_tweet_truncator(tweet)
   tweet.split(" ").collect do |phrase|
     if phrase.length > 140
       word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
     
 end
 
